d9220a6af18cda1b5f4442aea3e29f63a421d560
noshluk2 <noshluk2@gmail.com>
2023-06-01 10:49:09 +0500

    #9 Rtabmap and point cloud processing

---
+++ point_cloud_processing/src/p9_a_table_scene.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <filesystem>
+
+int main(){
+    pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2());
+    pcl::PCDReader cloud_reader;
+    std::string pcd_file_name = "table_scene.pcd";
+	std::filesystem::path ros2_ws_path    = std::filesystem::current_path();
+    std::filesystem::path point_cloud_dir = ros2_ws_path/"src/point_cloud_processing/point_clouds/";
+    std::string pcd_file_path             = point_cloud_dir/pcd_file_name;
+
+    cloud_reader.read(pcd_file_path,*cloud);
+
+    std::cout<<"Number of point "<< cloud->width * cloud->height << std::endl;
+
+    return 0;
+}
---
+++ point_cloud_processing/src/p9_b_planner_cloud.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <filesystem>
+int main()
+{
+    pcl::PointCloud<pcl::PointXYZ> cloud ;
+
+    cloud.push_back(pcl::PointXYZ(1.0 ,2.0 ,3.0));
+    cloud.push_back(pcl::PointXYZ(4.0 ,5.0 ,6.0));
+    cloud.push_back(pcl::PointXYZ(7.0 ,8.0 ,9.0));
+    cloud.push_back(pcl::PointXYZ(10.0,11.0,12.0));
+    cloud.push_back(pcl::PointXYZ(12.0,14.0,-15.0));
+
+    std::string pcd_file_name = "plane_.pcd";
+	std::filesystem::path ros2_ws_path    = std::filesystem::current_path();
+    std::filesystem::path point_cloud_dir = ros2_ws_path/"src/point_cloud_processing/point_clouds/";
+    std::string pcd_file_path             = point_cloud_dir/pcd_file_name;
+
+    pcl::io::savePCDFileASCII(pcd_file_path,cloud);
+    std::cout<<cloud.size();
+
+
+    return 0;
+
+}
